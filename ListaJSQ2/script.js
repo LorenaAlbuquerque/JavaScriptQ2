46f0e35f6ba0f9c2755b7ef52b465bf8279fe55b
LorenaAlbuquerque <92889078+LorenaAlbuquerque@users.noreply.github.com>
2023-09-06 09:17:40 -0300

    Add files via upload

---
+++ ListaJSQ2/script.js
@@ -0,0 +1,8 @@
+var r = prompt("Insira um número inteiro");
+  if (r>0) {
+    document.write("Número é positivo");
+}else if(r<0){
+    document.write("Número é negativo");
+} else{
+  document.write("Número é 0");
+}
